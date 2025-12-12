131a4589c22ed0cdf7415b7e39b865dd1d4abc98
Wyatt <gaudin118@wearescpps.org>
2025-12-12 15:01:15 +0000

    mandatory push

---
+++ extra-projects/roulette/roulette.js
@@ -0,0 +1,19 @@
+var wheel = jQuery("#wheel");
+setInterval(rotateImage, 1)
+var rotationSpeed = 10;
+var angle = 0;
+
+function rotateImage() {
+    // Increment the angle
+    angle += rotationSpeed;
+    if (angle >= 360){
+      angle -= 360;
+    }
+    wheel.css("transform",  "rotate(" + angle + "deg)");
+    wheel.css("position", "absolute");
+    wheel.css("top", "50%"); 
+    wheel.css("left", "50%");
+    // wheel.css("transform", "translate(-50%, -50%)");
+}
+
+rotateImage();
